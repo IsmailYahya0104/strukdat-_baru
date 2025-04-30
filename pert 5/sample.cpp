6d449a214e203971500d49e4a724c880917432a7
root <root@DESKTOP-NL130D9.>
2025-04-30 16:06:27 +0700

    menambahkan pert 5

--- pert 5/sample.cpp
+++ pert 5/sample.cpp
@@ -4,13 +4,26 @@ using namespace std;
 int tambah(int a, int b) { return a + b; }
 int kurang(int a, int b) { return a - b; }
 int kali(int a, int b) { return a * b; }
-float bagi(int a, int b) { return b != 0 ? (float)a/b : 0; }
+float bagi(int a, int b) { return b != 0 ? (float)a / b : 0; }
 
 int main() {
-    cout << "5 + 10 = " << tambah(5, 10) << endl;
-    cout << "10 - 5 = " << kurang(10, 5) << endl;
-    cout << "5 * 10 = " << kali(5, 10) << endl;
-    cout << "10 / 3 = " << bagi(10, 3) << endl;
+    int a, b;
+    
+    cout << "Masukkan bilangan pertama: ";
+    cin >> a;
+    cout << "Masukkan bilangan kedua: ";
+    cin >> b;
+    
+    cout << a << " + " << b << " = " << tambah(a, b) << endl;
+    cout << a << " - " << b << " = " << kurang(a, b) << endl;
+    cout << a << " * " << b << " = " << kali(a, b) << endl;
+    
+    if (b != 0) {
+        cout << a << " / " << b << " = " << bagi(a, b) << endl;
+    } else {
+        cout << "Pembagian oleh nol tidak valid." << endl;
+    }
+    
     return 0;
 }
 
